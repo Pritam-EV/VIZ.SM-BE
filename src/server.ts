0168b5a6278a7014f062dbacb06d9b7cadccd94c
Pritam-EV <pritam.ev@gmail.com>
2025-11-20 01:14:29 +0530

    Initial commit of project folde5

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import MqttService from "./Services/Mqtt/Service.js";
 console.info("Starting application ...");
 
 configureMongoDb();
-await configureMqttAsync();
+// await configureMqttAsync();
 const app = getApp(); // Express app
 const port = Number(process.env.PORT || 5000);
 
@@ -25,4 +25,4 @@ const server = app.listen(port, (error) => {
     console.log(`✅ API:https://smartmeter-vjratechnologies.web.app/api/account/signup/user`);
 });
 
-MqttService.startProcessingIncommingMessages();
+// MqttService.startProcessingIncommingMessages();
