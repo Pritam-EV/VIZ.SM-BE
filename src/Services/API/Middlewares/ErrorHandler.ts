fd29a2b0f5fd8568785a9f1a29be37f1dde67c95
Pritam-EV <pritam.ev@gmail.com>
2026-01-20 03:47:46 +0530

    cashfree dev-1

--- src/Services/API/Middlewares/ErrorHandler.ts
+++ src/Services/API/Middlewares/ErrorHandler.ts
@@ -1,11 +1,12 @@
 import type { Request, Response, NextFunction } from "express";
 import { isHttpError } from "http-errors";
-import { AlertError, HttpError } from "../../../Shared/Common/CustomErrors/HttpErrors.js";
+import { AlertError, HttpError, ResponseStatus } from "../../../Shared/Common/CustomErrors/HttpErrors.js";
 import { toAlertType } from "../../../Shared/Common/Enums/AlertTypes.js";
 import { Alert, AlertsResponse, BaseMessageResponse } from "../../../Shared/Common/Models/Responses.js";
 import type { RequestWithLoggerOnly } from "../../../Shared/Common/Types/ApiTypes.js";
 import type { LoggableType } from "../../../Shared/Common/Types/LoggableTypes.js";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default async function handleError(err: unknown, req: Request, res: Response, next: NextFunction) {
     try {
         if ((req as RequestWithLoggerOnly).customContext?.logger) {
@@ -15,7 +16,7 @@ export default async function handleError(err: unknown, req: Request, res: Respo
             console.error(err, "Something went wrong", "Request:", req);
         }
 
-        if (typeof err === "object" && err instanceof Error && !isHttpError(err)) {
+        if (typeof err === "object" && err instanceof Error) {
             if (err instanceof HttpError) {
                 if (err.doExpose) {
                     if (err instanceof AlertError) {
@@ -29,17 +30,19 @@ export default async function handleError(err: unknown, req: Request, res: Respo
                     res.status(err.statusCode).end();
                 }
             }
-            else {
-                res.status(500).send("Something went wrong");
+            else if (isHttpError(err)) {
+                res.status(err.statusCode).send(`${(err.expose ? err.message : "Something went wrong.")}`);
             }
-        }
-        else {
-            next(err);
         }
     }
     catch (error) {
         console.error("An error occured while handling the previous error.", { newError: error, previousError: err });
+    }
 
-        next(err);
+    try {
+        res.status(ResponseStatus.InternalServerError).send("Something went wrong.");
+    }
+    catch {
+        res.end();
     }
 }
